9e97b7887d8d5372bf5e822fe95c06ae41f3fe28
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2024-06-21 14:34:20 +0200

    chore: Update footer in backOfiice.blade.php and baseForm.blade.php

--- resources/views/backOfiice.blade.php
+++ resources/views/backOfiice.blade.php
@@ -152,6 +152,32 @@
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4" style="overflow-y: auto!important; height:100%">
                 @yield('content')
 
+                {{-- Footer of the page --}}
+                <div class="container mt-5">
+                    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                        <p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 Company, Inc</p>
+
+                        <a href="/"
+                            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                            <svg class="bi me-2" width="40" height="32">
+                                <use xlink:href="#bootstrap" />
+                            </svg>
+                        </a>
+
+                        <ul class="nav col-md-4 justify-content-end">
+                            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a>
+                            </li>
+                            <li class="nav-item"><a href="#"
+                                    class="nav-link px-2 text-body-secondary">Features</a></li>
+                            <li class="nav-item"><a href="#"
+                                    class="nav-link px-2 text-body-secondary">Pricing</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a>
+                            </li>
+                            <li class="nav-item"><a href="#"
+                                    class="nav-link px-2 text-body-secondary">About</a></li>
+                        </ul>
+                    </footer>
+                </div>
             </main>
         </div>
     </div>
